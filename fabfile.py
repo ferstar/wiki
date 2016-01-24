5e5a216b0ffd4285658bb482462fac075035742e
ferstar <zhangjianfei3@gmail.com>
2016-01-24 11:34:00 +0800

    Update Documentation

--- fabfile.py
+++ fabfile.py
@@ -77,13 +77,13 @@ def deploy_git(deploy_configs):
     branch = deploy_configs.get('branch', 'gh-pages')
     # commit gh-pages branch and push to remote
     _mesg = 'Auto Update Site By Fabric'
-    local('cd %s' % output_dir)
-    local('git add .', capture=False)
-    local('git commit -m "{0}"'.format(_mesg), capture=False)
-    local('git push')
-    local('cd ..')
-    #local('ghp-import -p -m "{0}" -r {1} -b {2} {3}' \
-    #      .format(_mesg, remote, branch, output_dir), capture=False)
+    #local('cd %s' % output_dir)
+    #local('git add .', capture=False)
+    #local('git commit -m "{0}"'.format(_mesg), capture=False)
+    #local('git push')
+    #local('cd ..')
+    local('ghp-import -p -m "{0}" -r {1} -b {2} {3}' \
+          .format(_mesg, remote, branch, output_dir), capture=False)
 
 
 def deploy_ftp(deploy_configs):
